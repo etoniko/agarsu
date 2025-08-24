883619f57900e71e1af3d7a4fc90203ad6b3ef26
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2025-08-25 02:29:04 +0300

    Update main_out.js

--- main_out.js
+++ main_out.js
@@ -2959,7 +2959,6 @@ const onLogout = () => {
     // Обновляем кнопки
     logoutButton.style.display = "none";
     loginButton.style.display = "";
-    telegram-login-agarsu_auth_bot.style.display = '';
 showLogoutNotification();
 };
 
@@ -3016,7 +3015,6 @@ const setAccountData = data => {
     document.querySelectorAll(".menu-item")[2].click(); // На главную меню
     logoutButton.style.display = "";
     loginButton.style.display = "none";
-    telegram-login-agarsu_auth_bot.style.display = 'none';
 };
 
 const loadAccountUserData = async () => {
