f45e13060bfb75590ed0bcdffda097ce1f5fd3fe
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2025-09-04 00:28:26 +0300

    Update main_out.js

--- assets/scripts/main_out.js
+++ assets/scripts/main_out.js
@@ -2500,7 +2500,7 @@ function drawLeaderBoard() {
     // var data = {
     //     "action": "test"
     // };
-    var transparent = ["незнакомка","bublik"];
+    var transparent = ["незнакомка","bublik","ник"];
     var
         // delay = 500,
         oldX = -1,
