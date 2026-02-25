bfd507bf8b7da22c23cf83e000786f378fc87f32
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-02-25 23:39:16 +0300

    Update shop.js

--- assets/scripts/shop.js
+++ assets/scripts/shop.js
@@ -249,12 +249,12 @@ function calculateCost() {
     return;
   }
 
-  let passwordCost = password ? 50 : 0;
-  let invisibleCost = invisibleNickCheckbox.checked ? 250 : 0;
+  let passwordCost = password ? 100 : 0;
+  let invisibleCost = invisibleNickCheckbox.checked ? 500 : 0;
   let skinCost = 0;
   let skinText = 'Скин: 0 ₽';
   if (file) {
-    skinCost = file.type === 'image/gif' ? 5000 : 50;
+    skinCost = file.type === 'image/gif' ? 10000 : 100;
     skinText = `Скин: ${skinCost * multiplier} ₽ (${file.type === 'image/gif' ? 'GIF' : 'PNG/JPG'})`;
   }
     const total = (passwordCost + skinCost + invisibleCost) * multiplier;
@@ -400,5 +400,3 @@ togglePassword.addEventListener("click", () => {
 
 
 invisibleNickCheckbox.addEventListener("change", calculateCost);
-
-
