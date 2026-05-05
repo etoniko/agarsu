ddb21311d30b73c4edd4f7ea0487a6600775f51e
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-05-05 20:18:21 +0300

    Update main_out.js

--- assets/scripts/main_out.js
+++ assets/scripts/main_out.js
@@ -202,6 +202,7 @@ wHandle.startGame = function () {
     };
 
 const SERVERS = {
+        "ffa":   "beget.agar.su:6001",
 		"agario":   "ffa.agar.su:6001",
 		"ffasolo":    "ffa.agar.su:6008",
         "ms":    "ffa.agar.su:6002",
