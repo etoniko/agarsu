35dfb910a582af00465ba6ddb9924ccb656d2ba5
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-05-12 20:05:41 +0300

    Update main_out.js

--- assets/scripts/main_out.js
+++ assets/scripts/main_out.js
@@ -1389,13 +1389,6 @@ function wsConnect(undefined) { // Убираем параметр token
         ws.send(a.buffer)
     }
 
-    function httpGet(theUrl) {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", theUrl, false); // false for synchronous request
-        xmlHttp.send(null);
-        return xmlHttp.responseText;
-    }
-
 
 let ping = 0;    
 let pingstamp = 0;
