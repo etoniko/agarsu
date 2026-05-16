b2d236d81118b01a07c1f81e69ad998a768fe4ea
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-05-16 16:49:53 +0300

    Update main_out.js

--- assets/scripts/main_out.js
+++ assets/scripts/main_out.js
@@ -67,9 +67,7 @@ stats.forEach((player, index) => {
             // Если сервер ещё не выбран руками — ставим его
             if (!SELECTED_SERVER) {
                 SELECTED_SERVER = activeLi.dataset.ip;
-				CONNECTION_URL = activeLi.dataset.ip;
             }
-			wHandle.chekstats();
         }
     }
 
@@ -220,11 +218,9 @@ document.querySelectorAll('.gamemode li').forEach(li => {
 
         // Запоминаем выбранный сервер
         SELECTED_SERVER = li.dataset.ip;
-		CONNECTION_URL = li.dataset.ip; 
         // Обновляем hash без дергания страницы
         history.replaceState(null, '', '#' + li.id);
 titleEl.textContent = `Статистика ${li.id}`;
-		chekstats();
         // ✅ Если сервер уже был активным — сразу стартуем игру
         if(isAlreadyActive) {
             wHandle.startGame();
@@ -255,7 +251,7 @@ function initServers() {
     document.querySelectorAll('.gamemode li').forEach(li => li.classList.remove('active'));
     const activeLi = document.getElementById(serverKey);
     if (activeLi) activeLi.classList.add('active');
-    chekstats();
+    wHandle.chekstats();
     // НОВЫЙ КОД: автоматический запуск нужного режима через wHandle.onload
     if (urlParams.has('spect') || hash.includes('?spect')) {
         // Не трогаем zoom здесь, он установится в gameLoop
