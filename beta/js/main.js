d775ba5116562e1d04e0814a2089c922fd4651c2
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-05-29 23:00:03 +0300

    Delete beta directory

--- beta/js/main.js
+++
@@ -1,9 +0,0 @@
-import { installArrayExtensions } from "./utils/extensions.js";
-import { Core } from "./core/Core.js";
-
-installArrayExtensions();
-
-const core = new Core();
-globalThis.CORE = core;
-
-export { core as CORE };
