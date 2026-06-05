1ba7ebfa6825979555a6526b1069b4e47136d75b
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-06-05 17:56:01 +0300

    Delete beta directory

--- beta/js/render/pixi.js
+++
@@ -1,6 +0,0 @@
-export function getPixi() {
-    if (!globalThis.PIXI) {
-        throw new Error("PIXI is not loaded. Include pixi.js before the game modules.");
-    }
-    return globalThis.PIXI;
-}
