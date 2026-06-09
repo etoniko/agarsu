f1225f322431f3e84e28aa7df794a1e4afa81502
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-06-09 13:32:25 +0300

    Delete stats directory

--- stats/config.js
+++
@@ -1,8 +0,0 @@
-/** API на api.agar.su:6009 */
-window.STATS_API = "https://api.agar.su:6009";
-
-/** Ссылка на профиль игрока (id = номер строки в pass.txt) */
-window.statsProfileUrl = function (id) {
-  const dir = location.href.replace(/[^/]*$/, "");
-  return dir + "users/index.html?id=" + encodeURIComponent(id);
-};
