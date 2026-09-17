4be49779fbc5624e5817b96ef9ee206e9f591dea
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-09-17 21:36:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3814,7 +3814,7 @@
       ...d
     };
   }
-  var DEFAULT_TRANSPARENT = [ "liqwid", "⟨本⟩ Itana.", "†Ĵώâ4ќâ†","g","Uroboros","ww"];
+  var DEFAULT_TRANSPARENT = [ "liqwid", "⟨本⟩ Itana.", "†Ĵώâ4ќâ†","g","Uroboros","ww","demons"];
   function ensureNameSets(S) {
     if (!S.transparent) S.transparent = new Set(DEFAULT_TRANSPARENT);
     if (!S.invisible) {
