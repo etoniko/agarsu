86ebe63364704462dfb4c0d1fe87653aca9e1f0f
Niko Draselia <146545304+etoniko@users.noreply.github.com>
2026-09-21 00:29:55 +0300

    Delete slith directory

--- slith/js/utils/math.js
+++
@@ -1,8 +0,0 @@
-const getXp = level => ~~(100 * (level ** 2 / 2));
-
-const getLevel = xp => ~~((xp / 100 * 2) ** .5);
-
-const normalizeFractlPart = n => (n % (Math.PI * 2)) / (Math.PI * 2);
-
-
-export { getXp, getLevel, normalizeFractlPart };
